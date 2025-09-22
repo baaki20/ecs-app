64ccba766f9eccde952d6fb37260b4a0d0580820
baaki20 <huduabdulbaakijunior20@gmail.com>
2025-09-22 18:05:41 +0000

    update configurations

--- src/main/java/com/baaki/ecsapp/HomeController.java
+++ src/main/java/com/baaki/ecsapp/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("fullName", "Abdul Baaki N-Nyeyam Hudu");
         model.addAttribute("labName", "ECS CI/CD Lab");
-        model.addAttribute("lastUpdated", "Today");
+        model.addAttribute("lastUpdated", "New Today");
         return "index";
     }
 }
